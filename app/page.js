c77e2b7d75ffc5c2f739e57129b5f23d1b4bfaea
svper563 <btteachey@gmail.com>
2023-10-18 13:13:10 -0400

    Updated home

--- app/page.js
+++ app/page.js
@@ -1,13 +1,17 @@
 import Script from "next/script"
+import Intro from "./components/Intro";
 import CertIcons from "./components/CertIcons"
+import CertImages from "./components/CertImages";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <>
-
-      <h1 className='header'>Welcome home.</h1>
-      <p className='description'>I'll put useful stuff here, eventually.</p>
+      <Intro/>
+      <CertImages/>
       <CertIcons/>
+      <Footer/>
+     
 
       <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
       <Script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></Script>
