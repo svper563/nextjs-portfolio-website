55f7f9960d61382f64eb0a89403667618e3f1ac5
svper563 <btteachey@gmail.com>
2023-10-18 13:12:44 -0400

    Added intro component

---
+++ app/components/Intro.js
@@ -0,0 +1,12 @@
+export default function Intro() {
+    return (
+        <>
+            <h1>Welcome to teachey.dev</h1>
+            <p>IT professional.</p>
+            <p>Aspiring developer.</p>
+            <p>Mediocre chess player.</p>
+
+            <h2>BRIAN TEACHEY</h2>
+        </>
+    )
+}
