1f57f7d0772469ef5bc5797015fa8b54c4f0b3d2
svper563 <btteachey@gmail.com>
2023-10-18 13:12:55 -0400

    Added footer component

---
+++ app/components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+    return (
+        <>
+            <h3>Like what you see? Let's get connected.</h3>
+            <a href="https://www.linkedin.com/in/brian-teachey-003334212/" target="_blank" rel="noopener noreferrer">
+                <ion-icon name="logo-linkedin" title="Linkedin"></ion-icon>
+            </a>
+            <a href="https://github.com/svper563" target="_blank" rel="noopener noreferrer">
+                <ion-icon name="logo-github" title="Github"></ion-icon>
+            </a>
+            <a href="https://www.chess.com/member/user_name_here" target="_blank" rel="noopener noreferrer">
+                <ion-icon class="icon" name="game-controller" title="Chess.com"></ion-icon>
+            </a>
+
+            <p>Copyright 2023 Brian Teachey.</p>
+        </>
+    )
+}
