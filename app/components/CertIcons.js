b1055ec1b84b1fdea5a2ba2a03e5874177d7528e
svper563 <btteachey@gmail.com>
2023-10-18 13:12:27 -0400

    Updated CertIcons

--- app/components/CertIcons.js
+++ app/components/CertIcons.js
@@ -1,7 +1,13 @@
 export default function CertIcons() {
     return (
         <>
+            <ion-icon name="logo-html5" title="HTML 5"></ion-icon>
+            <ion-icon name="logo-css3" title="CSS 3"></ion-icon>
+            <ion-icon name="git-branch" title="git"></ion-icon>
             <ion-icon name="logo-nodejs" title="Node JS"></ion-icon>
+            <ion-icon name="logo-npm" title="NPM"></ion-icon>
+            <ion-icon name="logo-react" title="React"></ion-icon>
+            <ion-icon name="logo-python" title="Python"></ion-icon>
         </>
     )
 }
