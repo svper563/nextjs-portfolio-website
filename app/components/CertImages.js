a0dd6af7de5fcd6907ed3e421486bddfac6f9f5f
svper563 <btteachey@gmail.com>
2023-10-18 13:12:06 -0400

    Added cert images

---
+++ app/components/CertImages.js
@@ -0,0 +1,11 @@
+import Image from "next/image"
+
+export default function CertImages() {
+    return (
+        <>
+            <Image src="/Aplus-Logo-Certified-CE-White.png" width={100} height={100} alt="A+ Certified Logo"/>
+            <Image src="/NetworkPlus Logo Certified CE White.png" width={100} height={100} alt="A+ Certified Logo"/>
+            <Image src="/Python Coding Specialist.png" width={100} height={100} alt="A+ Certified Logo"/>
+        </>
+    )
+}
