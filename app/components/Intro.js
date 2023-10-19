1a8a459e5a041090acf9efd20160d6ac6f85e9b2
svper563 <btteachey@gmail.com>
2023-10-19 17:57:11 -0400

    Updated intro css module

--- app/components/Intro.js
+++ app/components/Intro.js
@@ -3,10 +3,10 @@ import styles from './Intro.module.css';
 export default function Intro() {
     return (
         <>
-            <h1>Welcome to teachey.dev</h1>
-            <p>IT professional.</p>
-            <p>Aspiring developer.</p>
-            <p>Mediocre chess player.</p>
+            <h1 className={styles.header}>Welcome to teachey.dev</h1>
+            <h2>IT professional.</h2>
+            <h2>Aspiring developer.</h2>
+            <h2>Mediocre chess player.</h2>
 
             <div className={styles.moon}>
                 <h2 className={styles.glitch}>BRIAN TEACHEY</h2>
