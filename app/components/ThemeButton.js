7f70fb68e80022b9517d796945d72c8eb8dd01b0
svper563 <btteachey@gmail.com>
2023-10-22 21:16:28 -0400

    Initial theme toggle functionality

--- app/components/ThemeButton.js
+++ app/components/ThemeButton.js
@@ -1,10 +1,18 @@
-// import styles from "./Navbar.module.css";
+'use client'
+import { useState } from "react";
 
 export default function ThemeButton({className}) {
+
+    const [theme, setTheme] = useState("dark");
+
+    function toggleTheme(theme) {
+        theme === "dark" ? setTheme("light") : setTheme("dark");
+    }
+
     return (
         <>
-            <button className={className}>
-                <ion-icon name="sunny-outline"/>
+            <button className={className} onClick={() => toggleTheme(theme)}>
+                {theme == "dark" ? <ion-icon name="moon-outline"/> : <ion-icon name="sunny-outline"/>}
             </button>
         </>
     )
