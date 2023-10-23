000c028ab31bc9c8922ba29560d1b3144434f560
svper563 <btteachey@gmail.com>
2023-10-22 21:37:47 -0400

    Adjusted cert alt descriptions

--- app/components/CertImages.js
+++ app/components/CertImages.js
@@ -5,8 +5,8 @@ export default function CertImages() {
     return (
         <div className={styles.certs}>
             <Image className={styles.cert} src="/Aplus-Logo-Certified-CE-White.png" width={300} height={300} alt="A+ Certified Logo"/>
-            <Image className={styles.cert} src="/NetworkPlus Logo Certified CE White.png" width={300} height={300} alt="A+ Certified Logo"/>
-            <Image className={styles.cert} src="/Python Coding Specialist.png" width={300} height={300} alt="A+ Certified Logo"/>
+            <Image className={styles.cert} src="/NetworkPlus Logo Certified CE White.png" width={300} height={300} alt="Network+ Certified Logo"/>
+            <Image className={styles.cert} src="/Python Coding Specialist.png" width={300} height={300} alt="Python Coding Specialist Logo"/>
         </div>
     )
 }
